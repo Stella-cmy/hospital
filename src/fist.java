18305250f2dca7eb5de8e369e87d9790f97e3ee2
Stella-cmy <2680462740@qq.com>
2020-11-03 12:27:27 +0800

    second commit

--- src/fist.java
+++ src/fist.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class fist {
-    String fist;
+    static String fist;
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        fist = sc.next();
+        System.out.println(fist);
     }
 }
